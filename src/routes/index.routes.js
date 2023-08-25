fd39e79b53d92b78d985c5d04ff5d4d86b1f8421
pabrax <pabloeg0420@outlook.com>
2023-08-25 14:23:26 -0500

    Feature: update, done & delete tasks

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -12,8 +12,35 @@ router.get("/about", (req, res) => {
   res.render("about.hbs");
 });
 
-router.get("/edit", (req, res) => {
-  res.render("edit.hbs");
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await Tasks.findByIdAndDelete(id, req.body);
+  res.redirect("/");
+});
+
+router.get("/toggleDone/:id", async (req, res) => {
+  const { id } = req.params;
+  const task = await Tasks.findById(id);
+
+  task.done = !task.done;
+  await task.save();
+  res.redirect("/");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const task = await Tasks.findById(req.params.id).lean();
+    res.render("edit.hbs", { task: task });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  await Tasks.findByIdAndUpdate(id, req.body);
+
+  res.redirect("/");
 });
 
 router.post("/tasks/add", async (req, res) => {
